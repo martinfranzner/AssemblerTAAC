175a94696aea4b751ab17a096323cbb5c1eaa0b6
Eduardo Borsa <borsa.dado@gmail.com>
2017-09-21 11:45:06 -0300

    Eduardo first commit
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- DictionaryMneumonic.py
+++ DictionaryMneumonic.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  5 15:17:55 2017
-
-@author: martinfranzner
-"""
-
-#dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-#print ("dict['Name']: ", dict['Name'])
-#print ("dict['Age']: ", dict['Age'])
 
 import re 
-
 
 def createDic(file):
     dic = {}
@@ -41,13 +31,8 @@ if __name__ == "__main__":
              'SUB':8, 'CMP':9, 'JMP':10, 'JZ':11, 'OUT':12,
              'INC':13, 'DEC':14, 'MUL':15,'DIV':16}
     """
-    
-    
-
-    
-    
-    path_opcode= ('/Users/martinfranzner/Documents/PUC COMPUTAÇÃO/4o semestre/Arquitetura Computadores 2/Assembler/OPCODES.esym.txt') 
-    path_reg= ('/Users/martinfranzner/Documents/PUC COMPUTAÇÃO/4o semestre/Arquitetura Computadores 2/Assembler/REGCODES.esym.txt') 
+    path_opcode= ('./OPCODES.esym.txt') 
+    path_reg= ('./REGCODES.esym.txt') 
     
     days_file_op = open(path_opcode,'r')
     aux_op = days_file_op.readlines()
