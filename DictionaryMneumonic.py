3b06a031dc1e1c129bbeef93a248f7ba9e5be254
Eduardo Borsa <borsa.dado@gmail.com>
2017-09-29 09:29:44 -0300

    Modificacoes de sexta
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- DictionaryMneumonic.py
+++ DictionaryMneumonic.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  5 15:17:55 2017
-
-@author: martinfranzner
-"""
-
-#dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-#print ("dict['Name']: ", dict['Name'])
-#print ("dict['Age']: ", dict['Age'])
-
-import re 
+import re
 
 
-def createDic(file):
+def createDictionary(file):
     dic = {}
-    i = 0;
+    counter = 0;
     for line in file:
-        string = re.sub(r'\s+', '', line)
-        if(string.rsplit(';', 1)[0] != ''):
-            dic[string.rsplit(';', 1)[0]] = i
-            i += 1
+        string = re.sub(r'\s+', '', line)  # Remove spaces
+        if string.rsplit(';', 1)[0] != '':
+            dic[string.rsplit(';', 1)[0]] = counter
+            counter += 1
     return dic
+
 
 def register(file):
     regdic = {}
@@ -29,133 +20,134 @@ def register(file):
     for line in file:
         string = re.sub(r'\s+', '', line)
         regdic[string] = i
-        i+= 1
-        
+        i += 1
     return regdic
 
+
 def createInstructions(file, dicionario, registradores):
-    vetor=[]
-    comandoVet=[]
-    auxVet=[]
-    auxComandos = []
-    registrador_registrador = 0
-    addr = 0
+    vectorComand = []
+    comands_aux = []
+    register = 0
+    index = 0
     immediate = 0
-    auxCount = 0
+    aux_count = 0
     isRegAddres = False
-    
-    i=0
-    temMov = True
+
     for line in file:
-        string = re.sub(r'\s+', ' ', line)
-        if(string.rsplit(';', 1)[0] != ''):
-            #print(string.rsplit(';',1)[0])
-            if(string.rsplit(';',1)[0] != 'MOV'):
-                commands = (string.rsplit(';',1)[0])
+        string = re.sub(r'\s+', ' ', line)  # Remove espaços
+        if (string.rsplit(';', 1)[0] != ''):
+            if (string.rsplit(';', 1)[0] != 'MOV'):  # verifica se é MOV, se nao printa o comando
+                commands = (string.rsplit(';', 1)[0])
                 commands = commands.replace(',', ' ')
-                #print(commands)
                 command = commands.split(" ")
                 print(command)
-        
+        else:
+            command =[];
+
         if "MOV" in command:
             temMov = True
         else:
             temMov = False
-        
+
         for j in command:
             if not temMov:
                 if j in dicionario:
-                    comandoVet.append(dicionario[j])  
-                    #print(comandoVet)
+                    vectorComand.append(dicionario[j])
                 elif j in registradores:
-                    comandoVet.append(int(registradores[j]))
+                    vectorComand.append(int(registradores[j]))
                 elif '[' in j:
-                    comandoVet.append(int(j.strip("[]")))
+                    vectorComand.append(int(j.strip("[]")))
                 else:
-                    comandoVet.append(int(j))
+                    if j != '':
+                        vectorComand.append(int(j))
             else:
                 if j == "MOV":
-                    auxComandos.append(j)
+                    comands_aux.append(j)
                 elif j in registradores:
-                    registrador_registrador += 1
-                    auxComandos.append(int(registradores[j]))
+                    register += 1
+                    comands_aux.append(int(registradores[j]))
                 elif '[' in j:
-                    addr += 1
-                    auxComandos.append(int(j.strip("[]")))
+                    index += 1
+                    comands_aux.append(int(j.strip("[]")))
 
-                    if auxCount == 1:
+                    if aux_count == 1:
                         isRegAddres = False
                     else:
                         isRegAddres = True
                 else:
                     immediate += 1
-                    auxComandos.append(int(j))
-            
-            auxCount += 1
-            
-        for co in auxComandos:
-            if registrador_registrador == 2:
-                if co == "MOV":
-                    comandoVet.append(dicionario["MOV_RR"])
+                    if j != '':
+                        comands_aux.append(int(j))
+
+            aux_count += 1
+
+        for each in comands_aux:  # will just enter if it find MOV, otherwise comands_aux.size()=00
+            if register == 2:  # mover um registrador no outro
+                if each == "MOV":
+                    vectorComand.append(dicionario["MOV_RR"])
                 else:
-                    comandoVet.append(co)
-            elif registrador_registrador == 1 and addr == 1:
-                if co == "MOV":
+                    vectorComand.append(each)
+
+            elif register == 1 and index == 1:  # Para quando tem [NUM]
+                if each == "MOV":
                     if isRegAddres:
-                        comandoVet.append(dicionario["MOV_RM"])
+                        vectorComand.append(dicionario["MOV_RM"])
                     else:
-                        comandoVet.append(dicionario["MOV_MR"])
+                        vectorComand.append(dicionario["MOV_MR"])
                 else:
-                    comandoVet.append(co)
-            elif registrador_registrador == 1 and immediate == 1:
-                if co == "MOV":
-                    comandoVet.append(dicionario["MOV_RI"])
+                    vectorComand.append(each)
+
+            elif register == 1 and immediate == 1:  # Para quando tem somente num
+                if each == "MOV":
+                    vectorComand.append(dicionario["MOV_RI"])
                 else:
-                    comandoVet.append(co)
-            elif addr == 1 and immediate == 1:
-                if co == "MOV":
-                    comandoVet.append(dicionario["MOV_MI"])
+                    vectorComand.append(each)
+
+            elif index == 1 and immediate == 1:  # Para quando tem [NUM] e NUM
+                if each == "MOV":
+                    vectorComand.append(dicionario["MOV_MI"])
                 else:
-                    comandoVet.append(co)
-                    
-        registrador_registrador = 0
-        addr = 0
-        immediate = 0    
-        auxCount = 0
-        auxComandos.clear()
-        
-    return comandoVet
+                    vectorComand.append(each)
 
+        register = 0
+        index = 0
+        immediate = 0
+        aux_count = 0
+        comands_aux.clear()
 
+    return vectorComand
 
-    
 
 if __name__ == "__main__":
+    path_opcode = ('./OPCODES.esym.txt')
+    path_reg = ('./REGCODES.esym.txt')
+    path_instructions = ('./testeProf.txt')
+    # path_instructions = ('./testeInstructions.txt')
 
-    path_opcode= ('./OPCODES.esym.txt') 
-    path_reg= ('./REGCODES.esym.txt') 
-    path_instructions= ('./testeInstructions.txt') 
-    
-    
-    days_file_op = open(path_opcode,'r')
-    aux_op = days_file_op.readlines()
-    
-    days_file_reg = open(path_reg,'r')
-    aux_reg = days_file_reg.readlines()
-    
+    # RegCodes
+    reg_reader = open(path_reg, 'r')
+    aux_reg = reg_reader.readlines()
+
+    print("\n\n --Dicionario de Registradores--- \n")
     dici_reg = register(aux_reg)
     print(dici_reg)
-    
-    dici_op = createDic(aux_op)
+
+    # OpCodes
+    op_reader = open(path_opcode, 'r')
+    op_lines = op_reader.readlines()
+
+    print("\n\n ---Dicionario de OPCODES--- \n")
+    dici_op = createDictionary(op_lines)
     print(dici_op)
-    
-    days_file_instructions = open(path_instructions,'r')
-    aux_inst = days_file_instructions.readlines()
-    
-    vetor_instructions = createInstructions(aux_inst, dici_op, dici_reg)
-    
-    print(vetor_instructions)
-   
-   
 
+    print("\n\n-------\n\n")
 
+    # Instructions example
+
+    instructions_reader = open(path_instructions, 'r')
+    instr_lines = instructions_reader.readlines()
+
+    instructionsVector = createInstructions(instr_lines, dici_op, dici_reg)
+
+    print("\n\n ---OUTPUT instrucoes --- \n")
+    print(instructionsVector)
