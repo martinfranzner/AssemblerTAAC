c02b9ba9425e277c27c4eddfc71305a59d53a6b7
Eduardo Borsa <borsa.dado@gmail.com>
2017-11-01 19:19:32 -0200

    include tests
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

---
+++ TestesExemplo.cpp
@@ -0,0 +1,16 @@
+//
+// Created by eborsa on 01/11/17.
+//
+
+#include "catch.hpp"
+
+SCENARIO("Parte De Cache do Trabalho"){
+  GIVEN("Criando uma memoria") {
+    WHEN("fazemos qualquer merda") {
+      THEN("Coco") {
+        REQUIRE(1 == 1);
+      }
+    }
+  }
+}
+
